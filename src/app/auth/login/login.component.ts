d143bd54fabaffc920a3cc7d74828831044d996b
lathesram <lathesparan@gmail.com>
2023-02-03 00:19:12 +0530

    feat: :sparkles: basic login feature implemented

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  onLoginSubmit() {}
+}
